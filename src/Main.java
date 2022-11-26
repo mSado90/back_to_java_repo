4d35428449842bd959296b5111fca4056f5fc0f6
Michał Sadecki <michalsadecki90@gmail.com>
2022-11-26 11:36:19 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, testing returning back to Java ;)");
+
+        int a = 15;
+        double  b = 65.4;
+        String str = "aplauz";
+
+        System.out.println(a + b);
+    }
+
+}
